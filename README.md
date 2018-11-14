69ec7d4dae0a65e812ee16d738cdc798c0fd5a4a
blackmood <yuexingphp@gmail.com>
2018-11-14 11:10:49 +0800

    新增过渡效果

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ const list = [img1,img2]
 | speed | 播放速度 | 3000 |
 | pagination | 小圆点 | false |
 | navigation | 左右按钮 | false |
+| enterTimeout | 过渡时间 | 500 |
+| isAnimation | 是否启用动画 | true |
--- example/src/App.js
+++ example/src/App.js
@@ -2,21 +2,21 @@ import React from 'react'
 import SlideShow from '../../src/slideShow'
 
  class App extends React.Component{
-    
-     componentDidMount(){
-         
-     }
      render(){
          return (
-             <SlideShow 
-                speed={3000}
-                pagination={true}
-                navigation={true}
-             >
-                 <a href=""><img src="http://cdn.ydma.cn/2018-11-03-Km03e16Y" alt=""/></a>
-                 <a href=""><img src="http://cdn.ydma.cn/2018-08-21-SilFxmQF" alt=""/></a>
-                 <a href=""><img src="http://cdn.ydma.cn/2018-11-04-42HoHlgA" alt=""/></a>
-             </SlideShow>
+             <div>
+                <SlideShow 
+                    speed={3000}
+                    pagination={true}
+                    navigation={true}
+                    enterTimeout={500}
+                    isAnimation={true}
+                >
+                    <a href=""><img src="http://cdn.ydma.cn/2018-11-03-Km03e16Y" alt=""/></a>
+                    <a href=""><img src="http://cdn.ydma.cn/2018-08-21-SilFxmQF" alt=""/></a>
+                    <a href=""><img src="http://cdn.ydma.cn/2018-11-04-42HoHlgA" alt=""/></a>
+                </SlideShow>
+             </div>
          )
      }
  }
